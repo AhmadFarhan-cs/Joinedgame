cdcfaed27ef8db991bf2c321ff707b8fb33c168c
minigamerking <pocketmonsterlogin@gmail.com>
2024-08-09 17:21:20 -0400

    Added level 4
    Please help with levels, someone

--- New Game Project/scripts/door.gd
+++ New Game Project/scripts/door.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+@export var A_key : bool
+@export var B_key : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +10,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Playerstates.A_key== true and Playerstates.B_key == true:
-		queue_free()
+	if A_key and not B_key:
+		if Playerstates.A_key:
+			queue_free()
+	if B_key and not A_key:
+		if Playerstates.B_key:
+			queue_free()
+	if A_key and B_key:
+		if Playerstates.A_key and Playerstates.B_key:
+			queue_free()
